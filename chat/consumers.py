b973c2d4453627f80fd373c797ec006aa6f6330a
Samir <alijevsam@gmail.com>
2025-08-29 20:14:59 +0500

    first commit

---
+++ chat/consumers.py
@@ -0,0 +1,30 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class EchoConsumer(AsyncWebsocketConsumer):
+
+    async def connect(self):
+        await self.accept()
+
+    async def receive(self, text_data):
+        await self.send(text_data=text_data)
+
+    async def disconnect(self):
+        await self.disconnect()
+    
+class GlobalChatConsumer(AsyncWebsocketConsumer):
+
+    async def connect(self):
+        await self.channel_layer.group_add('global', self.channel_name)
+        await self.accept()
+
+    async def receive(self, text_data):
+        await self.channel_layer.group_send('global', {
+            'type': 'callback_for_chat_message',
+            'content': text_data
+        })
+
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard('global', self.channel_name)
+
+    async def callback_for_chat_message(self, event):
+        await self.send(text_data=event['content'])
---
+++ chat/routing.py
@@ -0,0 +1,8 @@
+from django.urls import re_path
+
+from chat.consumers import EchoConsumer, GlobalChatConsumer
+
+websocket_urlpatterns = [
+    re_path(r'ws/echo/', EchoConsumer.as_asgi()),
+    re_path(r'ws/chat/global/', GlobalChatConsumer.as_asgi()),
+]
