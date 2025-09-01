e5504b4a92eac5d79f72cb28e6b9c56e4a2c6196
Khamidkhanov Muhammadzohid <valihon555@gmail.com>
2025-09-01 15:57:53 +0500

    model.py

---
+++ chat/model.py
@@ -0,0 +1,38 @@
+from django.db import models 
+
+# class User(models.Model):
+#     username = models.CharField(max_length=150, unique=True)
+#     password = models.CharField(max_length=50)
+
+class Chat(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_1 = models.ForeignKey('auth.User', related_name='chats_initiated', on_delete=models.CASCADE)
+    user_2 = models.ForeignKey('auth.User', related_name='chats_received', on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    id = models.AutoField(primary_key=True)
+    text = models.TextField()
+    from_user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    chat = models.ForeignKey('Chat', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Message from {self.from_user} in chat {self.chat}"
+
+
+
+
+
+
+    
+
+    
+
+
+
+
+
+
