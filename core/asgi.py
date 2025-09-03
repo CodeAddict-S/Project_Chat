53a416e31edc7d2d0014be13e127e4ed5f4af41a
Samir <alijevsam@gmail.com>
2025-09-03 16:32:22 +0500

    fix asgi

--- core/asgi.py
+++ core/asgi.py
@@ -9,16 +9,19 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.security.websocket import AllowedHostsOriginValidator
-from chat.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+app = get_asgi_application()
+
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": app,
         "websocket": AllowedHostsOriginValidator(URLRouter(websocket_urlpatterns))
     }
 )
